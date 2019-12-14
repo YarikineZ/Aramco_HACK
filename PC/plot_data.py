6cab2f756468b96d127a4b3e45bdc7aff46bd857
Yaroslav Nasonov <yaroslav.nasonov@gmail.com>
2019-12-14 23:03:20 +0300

    Revert "Merge branch 'master' of https://github.com/YarikineZ/Aramco_HACK"
    This reverts commit 5b404c96c731113e15137f293033a87a0fec3960, reversing
    changes made to 8ca3d2bacb325cf391996f0d224b434308dd00f1.

--- PC/plot_data.py
+++ PC/plot_data.py
@@ -5,7 +5,7 @@ import msvcrt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
-
+from datetime import datetime
 
 
 def GetData(port):
@@ -17,45 +17,27 @@ def GetData(port):
         time = (int(reading[1]))
         noize = (int(re.sub(r'\r\n', '', reading[2])))
 
-        #data = np.append(data, [id, time, noize])
         data = np.array([id, time, noize]).reshape((1, 3))
-        #print("Func", data.shape)
         return data
     else:
         return 0
         #GetData(port)
 
-def myPlot(ser):
-    speed = 0.0001
-    fig, ax = plt.subplots(2, 2, figsize = (3, 3))
-    plt.ion()
-    #ax2 = plt.subplot(1, 2, 1)
-    for i in range(100):
-
-        #plt.axis([0, 7, 0, 20000])
-        data = GetData(ser)
-        print(data)
-        #plt.clf()
-        if(len(data)>3): ax[0, 0].plot(data[:, 2])
-        #plt.grid(True)
-        plt.show()
-        #plt.pause(speed)
-
-    ser.close()
 
 
 if __name__ == '__main__':
-    SERIAL_PORT = 'COM4'
+    SERIAL_PORT = 'COM9'
     SERIAL_RATE = 115200
     ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
 
     data = np.zeros((0, 3))
-    #print("Main", data.shape)
-    while(len(data)<10):
-        data = np.append(data, GetData(ser), axis=0)
-        print(len(data))
 
-    print(data)
-    plt.scatter(data[2], data[1], s=1)
+    init_time = datetime.now()
+
+    while((datetime.now() - init_time).seconds < 10):
+        data = np.append(data, GetData(ser), axis=0)
+        print(len(data), (datetime.now() - init_time).seconds)
+
+
+    plt.plot(data[:, 1], data[:, 2])
     plt.show()
-    #myPlot(ser)
